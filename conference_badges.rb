c45fd1d3cf59e18c8f55536186fb12e2e49645fb
Austin Powell <austinpowell58@gmail.com>
2018-08-08 17:45:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def batch_badge_creator(names)
+  names.each do |name|
+    
+  end 
+end
+
+def assign_rooms(speakers)
+  room = 1
+  speakers.each do |speaker|
+    "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    room += 1 
+  end 
+end 
